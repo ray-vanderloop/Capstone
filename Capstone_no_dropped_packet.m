3f5aa681c0368d30de994ec4edfd063c4b124617
khynes4 <48333007+khynes4@users.noreply.github.com>
2019-03-18 19:01:07 -0400

    Add files via upload

--- Capstone_no_dropped_packet.m
+++ Capstone_no_dropped_packet.m
@@ -19,6 +19,18 @@ TimeStamp4 =  [0 0 0 1];
 count = 1;
 used = [0 0 0 0 0 0];
 
+
+bits1 = 0;
+bits2 = "5555";
+bits3 = "5555";
+count1 = 0;
+count2 = 0;
+count3 = 0;
+state1 = 0;
+state2 = 0;
+state3 = 0;
+
+
 while(count <= DesiredNumGeneratedPackets)
 
    
@@ -91,6 +103,32 @@ while(count <= DesiredNumGeneratedPackets)
                 r = 5;
             end
             flight_state2 = horzcat(myZero, r); 
+            
+            if( isequal(flight_state2, [0 3]) )
+                count2 = count2 + 1;
+                bits1 = "0000" ; %set bits to low
+                if(count2 > 1)
+                    %state1 = 1;
+                    bits1 = "9999" ; %set bits to high
+                    if(count2 > 2) 
+                        %state1 = 2;
+                        bits1 = "0000";
+                    end
+                end
+            end
+            
+            if( isequal(flight_state2, [0 4]) )
+                count3 = count3 + 1;
+                bits1 = "0000" ; %set bits to low
+                if(count3 > 1)
+                    %state1 = 1;
+                    bits1 = "9999" ; %set bits to high
+                    if(count3 > 2) 
+                        %state1 = 2;
+                        bits1 = "0000";
+                    end
+                end
+            end
    
         else          
             flight_state2 = randi([0,9],1,2); %else set both bits (1x2 matrix) to random ints between 0 and 9
@@ -98,19 +136,35 @@ while(count <= DesiredNumGeneratedPackets)
   
         
         %all the rest of the N numbers = random Hex digits
-        length = N - 12;
-        myRandIntArray = randi ([1,16], 1, length);   %generate array of random ints between 0,15
-        myRandHexArray=  cell(1, length);
+        length = N - 32;
+        myRandIntArray1 = randi ([1,16], 1, length);   %generate array of random ints between 0,15
+        myRandHexArray1=  cell(1, length);
         
         i= 1;
         while(i <= length)
-            index = myRandIntArray(i)  ;
-            myRandHexArray(i) = cellstr( myHexOptions(index) ) ;
+            index = myRandIntArray1(i)  ;
+            myRandHexArray1(i) = cellstr( myHexOptions(index) ) ;
             i = i+1;
         end
         
-        myRandHexArray = char(myRandHexArray);
-        myRandHexArray = transpose(myRandHexArray);
+        myRandHexArray1 = char(myRandHexArray1);
+        myRandHexArray1 = transpose(myRandHexArray1);
+        
+        
+        %all the rest of the N numbers = random Hex digits
+        length = 12;
+        myRandIntArray2 = randi ([1,16], 1, length);   %generate array of random ints between 0,15
+        myRandHexArray2=  cell(1, length);
+        
+        i= 1;
+        while(i <= length)
+            index = myRandIntArray2(i)  ;
+            myRandHexArray2(i) = cellstr( myHexOptions(index) ) ;
+            i = i+1;
+        end
+        
+        myRandHexArray2 = char(myRandHexArray2);
+        myRandHexArray2 = transpose(myRandHexArray2);
         
         
         %convert all to char arrays
@@ -121,7 +175,7 @@ while(count <= DesiredNumGeneratedPackets)
         
         
         %concatenate into packet
-        myPacket = strcat(DeviceNum4, TimeStamp4_str, Packet_Type2, flight_state2_str, myRandHexArray)
+        myPacket = strcat(DeviceNum4, TimeStamp4_str, Packet_Type2, flight_state2_str, myRandHexArray1, bits1, bits2, myRandHexArray2)
         fwrite(s, myPacket);
         count = count + 1;
         
